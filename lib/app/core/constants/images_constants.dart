dcb65490ae4f7304f3fc718ccb18cff08bbbd06d
Caio Vellani <caiovellani@outlook.com>
2024-09-09 23:58:32 -0300

    refactor: change and update images

---
+++ lib/app/core/constants/images_constants.dart
@@ -0,0 +1,5 @@
+abstract class ImagesConstants {
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+}
