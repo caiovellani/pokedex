0b1d2060b8aeffaefbbecb35d77f9c87b77325f6
Caio Vellani <caiovellani@outlook.com>
2024-09-09 22:33:48 -0300

    remove: "remove unsual files"

--- lib/core/themes/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color splashBackground = Color(0xFF000029);
-  static const Color blue = Color(0xff173EA5);
-  static const Color lightBlue = Color(0xFf4565B7);
-}
