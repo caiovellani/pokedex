9e17f51d7041529a0084da2fc4357c6aae745084
Caio Vellani <caiovellani@outlook.com>
2024-09-09 22:34:34 -0300

    fix: move to main folder app.

---
+++ lib/app/core/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color splashBackground = Color(0xFF000029);
+  static const Color blue = Color(0xff173EA5);
+  static const Color lightBlue = Color(0xFf4565B7);
+}
