d1956c452d7c36a3ce2f2d9b54af1395ac7e22ba
Caio Vellani <caiovellani@outlook.com>
2024-09-09 23:58:50 -0300

    feat: add 2 more colors, grey6, and black

--- lib/app/core/themes/app_colors.dart
+++ lib/app/core/themes/app_colors.dart
@@ -4,4 +4,6 @@ class AppColors {
   static const Color splashBackground = Color(0xFF000029);
   static const Color blue = Color(0xff173EA5);
   static const Color lightBlue = Color(0xFf4565B7);
+  static const Color grey6 = Color(0xFF666666);
+  static const Color black = Colors.black;
 }
