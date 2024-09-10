c781f78cef1aa3e11fbc39fe24714deea2a434aa
Caio Vellani <caiovellani@outlook.com>
2024-09-09 23:58:59 -0300

    feat: create onboarding models

---
+++ lib/app/models/onboarding_model.dart
@@ -0,0 +1,32 @@
+class OnboardingContentModel {
+  final String image;
+  final String title;
+  final String description;
+
+  OnboardingContentModel({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
+
+List<OnboardingContentModel> contents = [
+  OnboardingContentModel(
+    title: 'Todos os Pokémons em um único lugar',
+    image: 'assets/images/onboarding1.png',
+    description:
+        'Acesse uma vasta lista de Pokémons de todas as gerações já feitas pela Nintendo.',
+  ),
+  OnboardingContentModel(
+    title: 'Mantenha sua Pokédex atualizada',
+    image: 'assets/images/onboarding2.png',
+    description:
+        'Cadastre-se e mantenha seu perfil, pokémon favoritos, configurações e muito mais, salvos no aplicativo, mesmo sem conexão com a internet.',
+  ),
+  OnboardingContentModel(
+    title: 'Está pronto para essa aventura?',
+    image: 'assets/images/onboarding3.png',
+    description:
+        'Basta criar uma conta e começar a explorar o mundo dos Pokémons hoje!',
+  ),
+];
