23cd261efb03e2ee238ec31677a182da574cb9e8
Caio Vellani <caiovellani@outlook.com>
2024-09-10 23:31:26 -0300

    feat: add login or register image page

--- lib/app/core/constants/images_constants.dart
+++ lib/app/core/constants/images_constants.dart
@@ -2,4 +2,5 @@ abstract class ImagesConstants {
   static const String onboarding1 = 'assets/images/onboarding1.png';
   static const String onboarding2 = 'assets/images/onboarding2.png';
   static const String onboarding3 = 'assets/images/onboarding3.png';
+  static const String loginOrRegister = 'assets/images/login_or_register.png';
 }
