6212c05997de19d4919fe81c82c4321c6f612b40
Caio Vellani <caiovellani@outlook.com>
2024-09-11 23:30:49 -0300

    refactor(routes): set initialLocation to splashscreen, and add LoginEmailPage route

--- lib/app/routes/router.dart
+++ lib/app/routes/router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 
+import '../pages/auth/login_email_page.dart';
 import '../pages/auth/login_or_register.dart';
 import '../pages/auth/login_page.dart';
 import '../pages/auth/register_page.dart';
@@ -8,7 +9,7 @@ import '../pages/splash_screen/splash_screen_page.dart';
 
 abstract class AppRouter {
   static final router = GoRouter(
-    initialLocation: OnboardingPage.route,
+    initialLocation: SplashScreenPage.route,
     routes: [
       GoRoute(
         path: SplashScreenPage.route,
@@ -29,6 +30,10 @@ abstract class AppRouter {
       GoRoute(
         path: RegisterPage.route,
         builder: (context, state) => const RegisterPage(),
+      ),
+      GoRoute(
+        path: LoginEmailPage.route,
+        builder: (context, state) => const LoginEmailPage(),
       ),
     ],
   );
