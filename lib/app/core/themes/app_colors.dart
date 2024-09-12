ec4cb783fee6bb9669f21aca4af1856c29e76a90
Caio Vellani <caiovellani@outlook.com>
2024-09-11 23:29:10 -0300

    feat: add shades of grey

--- lib/app/core/themes/app_colors.dart
+++ lib/app/core/themes/app_colors.dart
@@ -4,7 +4,11 @@ class AppColors {
   static const Color splashBackground = Color(0xFF000029);
   static const Color blue = Color(0xff173EA5);
   static const Color lightBlue = Color(0xFf4565B7);
-  static const Color grey6 = Color(0xFF666666);
+  static const Color grey300 = Color(0xFFB3B3B3);
+  static const Color grey400 = Color(0xFF999999);
+  static const Color grey500 = Color(0xFF808080);
+  static const Color grey600 = Color(0xFF666666);
+  static const Color grey700 = Color(0xFF4D4D4D);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 }
