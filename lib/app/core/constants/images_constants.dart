e08b8ee5a284bb70e67a0295763d9595409e1336
Caio Vellani <caiovellani@outlook.com>
2024-09-11 23:21:55 -0300

    feat: add apple and google logo

--- lib/app/core/constants/images_constants.dart
+++ lib/app/core/constants/images_constants.dart
@@ -3,4 +3,7 @@ abstract class ImagesConstants {
   static const String onboarding2 = 'assets/images/onboarding2.png';
   static const String onboarding3 = 'assets/images/onboarding3.png';
   static const String loginOrRegister = 'assets/images/login_or_register.png';
+  static const String loginPage = 'assets/images/login_page.png';
+  static const String appleLogo = 'assets/images/apple_logo.png';
+  static const String googleLogo = 'assets/images/google_logo.png';
 }
