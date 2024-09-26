d91238d11b855d9f10a613b176f0513a71243468
Caio Vellani <caiovellani@outlook.com>
2024-09-25 21:29:39 -0300

    feat: add images

--- lib/app/core/constants/images_constants.dart
+++ lib/app/core/constants/images_constants.dart
@@ -8,4 +8,16 @@ abstract class ImagesConstants {
   static const String googleLogo = 'assets/images/google_logo.png';
   static const String loginSuccess = 'assets/images/login_success.png';
   static const String createAccount = 'assets/images/create_account.png';
+  static const String successRegister = 'assets/images/success_register.png';
+  static const String backgroundRegister =
+      'assets/images/background_success_register.png';
+  static const String pokedex = 'assets/images/pokedex.png';
+  static const String pokedexGray = 'assets/images/pokedex_gray.png';
+  static const String pokedexRegions = 'assets/images/pokedex_regions.png';
+  static const String pokedexRegionsGray =
+      'assets/images/pokedex_regions_gray.png';
+  static const String favorites = 'assets/images/favorites.png';
+  static const String favoritesGray = 'assets/images/favorites_gray.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String profileGray = 'assets/images/profile_gray.png';
 }
